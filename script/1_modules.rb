bd40e339f2faead12d16ebbbd65852ea8f0fa510
DT_srialtha <sri.athota@dispatchtrack.com>
2024-07-05 13:14:40 +0530

    changes in 4 th program

--- script/1_modules.rb
+++ script/1_modules.rb
@@ -1,3 +1,23 @@
 module Cricket 
-    class 
+
+    class Store_team
+
+        def max_players
+        end
+
+        def add_player
+        end
+
+        def remove_player
+        end
+    end
+
+    class player
+        def add_runs
+        end
+
+        def wicket_count
+            
+        end
+    end
 end
--- script/4_csv_file.rb
+++ script/4_csv_file.rb
@@ -12,23 +12,35 @@ def strike_rate
     s =  CSV.read("/home/dispatchtrack/Desktop/T20_worldCup/script/sample_cricket_stats.csv")
     strike_rate_array = []
     sort = []
+    total_runs_scored = 0 
+    total_balls_faced = 0
     
-    begin
     for i in 1..(s.size)
         sr = (s[i][1].to_f)/(s[i][2].to_f)
         strike_rate_array << sr
-        puts "Strike rate of #{s[i][0]} is #{sr}"
-        
-    end
-    rescue StandardError => error
-        puts "Error in calculating strike rate"
+        total_runs_scored += s[i][1].to_f
+        total_balls_faced += s[i][2].to_f
+        puts "Strike rate of #{s[i][0]} is #{sr}" 
     end
     #return strike_rate_array
     sort[] = strike_rate_array.sort
-
     p "The Highest strike rate is #{sort[0]}"
+    p "The total runs scored by all plyers is #{total_runs_scored}"
 end
 p strike_rate()
 def print
-    
-end
+end  
+
+=begin
+begin   
+    for i in 1..(s.size)
+      sr = (s[i][1].to_f)/(s[i][2].to_f)
+      #strike_rate_array << sr
+      total_runs_scored += s[i][1]
+      total_balls_faced += s[i][2]
+        #puts "Strike rate of #{s[i][0]} is #{sr}"  
+    end
+    rescue StandardError => error
+      puts "Error in calculating strike rate"
+  end
+=end
